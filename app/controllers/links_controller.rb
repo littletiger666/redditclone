055b5517b4118c18e98e815fd0f2b411f0a534c5
Xiaohu Li <li.xiaohu6@gmail.com>
2016-08-20 09:48:22 -0700

    Added and setup acts_as_votable

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -38,6 +38,18 @@ class LinksController < ApplicationController
     end
   end
 
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_to :back
+  end
+
   # PATCH/PUT /links/1
   # PATCH/PUT /links/1.json
   def update
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ApplicationRecord
   # Link is like post
+  acts_as_votable
   belongs_to :user
 end
